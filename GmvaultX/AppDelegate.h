fc53937922af8510a5738626aec1ad1e3b950271
Christian Kündig <christian@christiansmbp6.lan>
2014-01-07 03:11:08 +0100

    day1

--- GmvaultX/AppDelegate.h
+++ GmvaultX/AppDelegate.h
@@ -1,17 +1,47 @@
 //
 //  AppDelegate.h
-//  GmvaultX
+//  TasksProject
 //
-//  Created by christian on 06/01/14.
-//  Copyright (c) 2014 christian kuendig. All rights reserved.
+//  Created by Andy on 3/23/13.
+//  Copyright (c) 2013 Ray Wenderlich. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-@property (weak) IBOutlet NSTextField *textField;
 
 @property (assign) IBOutlet NSWindow *window;
 
-- (IBAction)buttonName:(id)sender;
+@property (weak) IBOutlet NSToolbarItem *tabButton1;
+@property (weak) IBOutlet NSToolbarItem *tabButton2;
+@property (weak) IBOutlet NSToolbarItem *tabButton3;
+@property (weak) IBOutlet NSTabView *tabView;
+
+@property  NSImage *tabImage1;
+@property  NSImage *tabImage2;
+@property  NSImage *tabImage3;
+
+@property  NSImage *tabImage1_selected;
+@property  NSImage *tabImage2_selected;
+@property  NSImage *tabImage3_selected;
+
+/**
+ * Project Package
+ */
+@property (unsafe_unretained) IBOutlet NSTextView *outputText;
+@property (weak) IBOutlet NSProgressIndicator *spinner;
+@property (weak) IBOutlet NSButton *buildButton;
+@property (weak) IBOutlet NSTextField *parameter;
+- (IBAction)startTask:(id)sender;
+- (IBAction)stopTask:(id)sender;
+
+/**
+ * NSTask 
+ */
+@property (nonatomic, strong) __block NSTask *buildTask;
+@property (nonatomic) BOOL isRunning;
+@property (nonatomic, strong) NSPipe *outputPipe;
+@property (nonatomic, strong) NSPipe *errorPipe;
+
+
 @end
--- GmvaultX/main.m
+++ GmvaultX/main.m
@@ -1,14 +1,14 @@
 //
 //  main.m
-//  GmvaultX
+//  TasksProject
 //
-//  Created by christian on 06/01/14.
-//  Copyright (c) 2014 christian kuendig. All rights reserved.
+//  Created by Andy on 3/23/13.
+//  Copyright (c) 2013 Ray Wenderlich. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, const char * argv[])
+int main(int argc, char *argv[])
 {
-    return NSApplicationMain(argc, argv);
+    return NSApplicationMain(argc, (const char **)argv);
 }
